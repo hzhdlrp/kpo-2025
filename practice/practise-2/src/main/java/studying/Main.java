7bfba964c3a5a5ce1e502e7b28fbe33ad32e528c
hzhdlrp <pswjft@gmail.com>
2025-01-24 20:48:36 +0300

    very-cool

--- practice/practise-2/src/main/java/studying/Main.java
+++ practice/practise-2/src/main/java/studying/Main.java
@@ -1,7 +1,38 @@
 package studying;
 
+import studying.domains.Customer;
+import studying.factories.HandCarFactory;
+import studying.factories.PedalCarFactory;
+import studying.params.EmptyEngineParams;
+import studying.params.PedalEngineParams;
+import studying.services.CarService;
+import studying.services.CustomerStorage;
+import studying.services.HseCarService;
+
+import static studying.params.EmptyEngineParams.DEFAULT;
+
 public class Main {
+
+    private HseCarService hseCarService;
+
     public static void main(String[] args) {
-        System.out.println("HSE");
+        CustomerStorage customerStorage = new CustomerStorage();
+
+        customerStorage.addCustomer(new Customer("customer1", 6, 4));
+        customerStorage.addCustomer(new Customer("customer2", 4, 6));
+        customerStorage.addCustomer(new Customer("customer1", 6, 6));
+        customerStorage.addCustomer(new Customer("customer2", 4, 4));
+
+        CarService carService = new CarService();
+        HandCarFactory handCarFactory = new HandCarFactory();
+        PedalCarFactory pedalCarFactory = new PedalCarFactory();
+
+        carService.addCar(handCarFactory, DEFAULT);
+        carService.addCar(handCarFactory, DEFAULT);
+
+        carService.addCar(pedalCarFactory, new PedalEngineParams(6));
+        carService.addCar(pedalCarFactory, new PedalEngineParams(6));
+
+
     }
 }
