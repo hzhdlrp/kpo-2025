583baecd73cecb52da6fa1b4b90b864db58f28ee
hzhdlrp <pswjft@gmail.com>
2025-06-13 01:08:58 +0300

    some changes :)

--- practice/hw/hw3/src/gateway/src/main/java/org/example/controllers/AccountsController.java
+++ practice/hw/hw3/src/gateway/src/main/java/org/example/controllers/AccountsController.java
@@ -1,6 +1,7 @@
 package org.example.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
 
 @RestController
 @RequestMapping("/api/accounts")
+@Tag(name = "Accounts", description = "")
 public class AccountsController {
 
     @Autowired
